c395862b6451e73c25fc3d23f31bc43dee0432af
Yang XiaoJie <18858620615@163.com>
2017-02-18 17:31:01 +0800

    第一次提交

--- app/src/main/java/com/example/yxj/myapplication/MainActivity.java
+++ app/src/main/java/com/example/yxj/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Toast.makeText(MainActivity.this, "代码提交", 1).show();
             }
         });
 
